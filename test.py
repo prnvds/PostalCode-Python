42f0dd15df38805a5c1ece68d474b3d70ad3d824
Pranav <42324981+prnvds@users.noreply.github.com>
2019-04-21 22:36:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #===============================================================================================
 #Python application that allows a user to query and show details for a given UK postcode.
-#Test postcode to use: CB3 0FA (Featurespace Cambridge office)
-#Created By Pranav Desai ( prnv.ds@hotmail.com )
+#Test postcode to use: CB3 0FA 
+#Created By Pranav Desai ( prnv.ds@gmail.com )
 # Time taken : 1 Hr 22 Mins
 # Main Application file "test.py"
 # Unittest file "apitest.py"
